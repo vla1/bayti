68ef146a949b6cb63fc8bf14b08d970cefe5b83d
vla1 <vladis149@gmail.com>
2022-06-29 09:56:39 +0200

    remove navDrop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Navbar.css";
-import { Navbar, Container,Nav, NavDropdown} from "react-bootstrap";
+import { Navbar, Container,Nav} from "react-bootstrap";
 
 function NavbarHeader(){
 return(
